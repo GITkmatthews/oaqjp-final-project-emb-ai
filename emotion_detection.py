92c2ee3e97c7134fe5b095284599536025ea9ff6
GITkmatthews <matthews.kent@yahoo.com>
2025-09-01 15:52:37 -0400

    Commit changes for output

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests  # Import the requests library to handle HTTP requests
+import json
 def emotion_detector(text_to_analyze):  # Function definition. Takes string input (text_to_analyse)
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  # URL of the emotion detection service
     myobj = { "raw_document": { "text": text_to_analyze } }  # Create a dictionary with the text to be analyzed
@@ -6,10 +7,16 @@ def emotion_detector(text_to_analyze):  # Function definition. Takes string inpu
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
     return response.text  # Return the response text from the API
 
- # Parsing the JSON response from the API
+ #Parsing the JSON response from the API
     formatted_response = json.loads(response.text)
     # Extracting sentiment label and score from the response
-    label = formatted_response['emotionPredictions']['emotion']
-    score = formatted_response['emotionPredictions']['score']
+   
+    #anger = formatted_response[0:0]
+    #disgust = formatted_response[1:1]
+    #fear = formatted_response[2:2]
+    #joy = formatted_response[3:3]
+    #sadness = formatted_response[4:4]
+    #dominant_emotion = formatted_response['emotionPredictions'][0]
+    
     # Returning a dictionary containing sentiment analysis results
-    return {'emotion': label, 'score': score}
+    return {'emotion': emotion_prediction, 'score': score_prediction} 
