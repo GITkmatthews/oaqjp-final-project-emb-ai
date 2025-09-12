ebb37475121fdbbcc8d5d883eb4e8b3c99aadf52
GITkmatthews <matthews.kent@yahoo.com>
2025-09-12 08:15:10 -0400

    Changes for output

--- emotion_detection.py
+++ emotion_detection.py
@@ -5,18 +5,18 @@ def emotion_detector(text_to_analyze):  # Function definition. Takes string inpu
     myobj = { "raw_document": { "text": text_to_analyze } }  # Create a dictionary with the text to be analyzed
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
-    return response.text  # Return the response text from the API
-
- #Parsing the JSON response from the API
+    #Parsing the JSON response from the API
     formatted_response = json.loads(response.text)
-    # Extracting sentiment label and score from the response
-   
-    #anger = formatted_response[0:0]
-    #disgust = formatted_response[1:1]
-    #fear = formatted_response[2:2]
-    #joy = formatted_response[3:3]
-    #sadness = formatted_response[4:4]
-    #dominant_emotion = formatted_response['emotionPredictions'][0]
-    
+    emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
+    # Get dominate score from the response
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+    display_emotion_score = {
+    "anger": emotion_scores['anger'],
+    "disgust": emotion_scores['disgust'],
+    "fear": emotion_scores['fear'],
+    "joy": emotion_scores['joy'],
+    "sadness": emotion_scores['sadness'],
+    "dominant_emotion": dominant_emotion
+    }    
     # Returning a dictionary containing sentiment analysis results
-    return {'emotion': emotion_prediction, 'score': score_prediction} 
+    return display_emotion_score 
